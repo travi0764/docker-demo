4675f1da441e16a1fc9e11e729be757226c14658
Ravi Tiwari <ravitiwari@Ravis-MacBook-Air.local>
2021-11-21 19:12:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          HIi there
         </p>
+        <p>BYE THERE</p>
         <a
           className="App-link"
           href="https://reactjs.org"
